96b7a69d5ed97f7bc154c7f0e2696f9ad2d397ad
Karina Ramadhan <carinasofyan@gmail.com>
2019-02-12 15:17:22 +0800

    adding on key event lister to signin

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -37,6 +37,13 @@ class Signin extends React.Component {
 				})
 		}
 
+class Input extends React.Component {
+  		_handleKeyPress(e) {
+    		if (e.key === 'Enter') {
+      		console.log('do validate');
+    			}
+  			}
+
 	render() {
 		const {onRouteChange} = this.props;
 		return ( <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
@@ -67,6 +74,7 @@ class Signin extends React.Component {
 			    <div className="">
 			      <input 
 			      onClick = {this.onSubmitSignIn}
+			      onKeyPress={this._handleKeyPress}
 			      className ="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" 
 			      type ="submit" 
 			      value ="Sign in" />
