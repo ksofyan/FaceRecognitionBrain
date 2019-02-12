420ad945cf8114ae08fb1c3b49c358a35fdef346
Karina Ramadhan <carinasofyan@gmail.com>
2019-02-12 13:35:22 +0800

    fixing user name on rank

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -6,7 +6,7 @@ const Rank = ({name, entries}) => {
 	return (
 			<div>
 				<div className='white f2'>
-				 {'Karina, your current entry count is...'}
+				 {`${name}, your current entry count is...`}
 				</div>
 				<div className='white f1'>
 				 {entries}
