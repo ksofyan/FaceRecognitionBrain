dde1560606b39e3f5bbbe13fdcdae92e482ad0b6
Karina Ramadhan <carinasofyan@gmail.com>
2019-02-12 11:31:39 +0800

    fixing sigin route

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -19,7 +19,7 @@ class Signin extends React.Component {
 		}
 
 		onSubmitSignIn = () => {
-			fetch('https://warm-thicket-16897.herokuapp.com/signin', {
+			fetch('https://kssmartbrain.herokuapp.com/signin', {
 				method: 'post',
 				headers: {'Content-Type': 'application/json'},
 				body: JSON.stringify({
