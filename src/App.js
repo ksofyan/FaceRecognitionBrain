50d258bed4019837e7ea24186ffad82a8eb277d9
Karina Ramadhan <carinasofyan@gmail.com>
2019-02-12 13:54:13 +0800

    fixing rank issue

--- src/App.js
+++ src/App.js
@@ -85,7 +85,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('https://warm-thicket-16897.herokuapp.com/imageUrl', {
+    fetch('https://warm-thicket-16897.herokuapp.com/imageurl', {
             method: 'post',
             headers: {'Content-type': 'application/json'},
             body: JSON.stringify({
