52d795655311e1197bd61e7d9773de1fffd8085b
Karina Ramadhan <carinasofyan@gmail.com>
2019-02-21 11:26:39 +0800

    fixing logo and signout text

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -8,7 +8,7 @@ const Logo = () => {
 
 	return (
 
-		<div className = 'ma4 mt0'>
+		<div className = 'ma4 mt3 w-50 p4'>
 			<Tilt className="Tilt br6 shadow-6" options={{ max : 55 }} style={{height: 150, width: 150}} >
 	 			<div className="Tilt-inner pa3"> 
 	 				<img style={{paddingTop: '10px'}} alt='logo' src={brain}/> 
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,13 +4,14 @@ import Logo from '../Logo/Logo'
 const Navigation = ({onRouteChange, isSignedIn}) => {
 		if (isSignedIn){
 			return (
-				<div className="navbar">
-				<Logo />
-						<div className='signout'>
-							<p 
-							onClick = {()=> onRouteChange('signout')}
-							className = 'f3 link dim white underline pa3 pointer'> Sign Out </p>
+				<div className="cf">
+						<div className='fl w-50'>
+							<Logo />
 						</div>
+						<p
+						onClick = {()=> onRouteChange('signout')}
+						className = 'fl w-50 f3 link dim white underline pa3 pointer'> Sign Out
+						</p>
 			    </div>
 
 			);
@@ -31,18 +32,3 @@ const Navigation = ({onRouteChange, isSignedIn}) => {
 }
 
 export default Navigation;
-
-// <nav style ={{display:'flex', justifyContent:'flex-end'}}>
-// 					<p 
-// 					onClick = {()=> onRouteChange('signout')}
-// 					className = 'f3 link dim white underline pa3 pointer'> Sign Out </p>
-// 			    </nav>
-
-// <nav style ={{display:'flex', justifyContent:'flex-end'}}>
-// 					<p 
-// 					onClick = {()=> onRouteChange('signin')}
-// 					className = 'f3 link dim white underline pa3 pointer'> Sign in </p>
-// 					<p 
-// 					onClick = {()=> onRouteChange('register')}
-// 					className = 'f3 link dim white underline pa3 pointer'> Register</p>
-// 				</nav>
